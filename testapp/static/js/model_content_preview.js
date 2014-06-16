c54febb5eba95ec0bd2efa1f21770b7de5e02f9e
animasola <annima.sola@gmail.com>
2014-06-16 20:25:17 +0300

    Tables now can be updated with new entries

--- testapp/static/js/model_content_preview.js
+++ testapp/static/js/model_content_preview.js
@@ -24,18 +24,19 @@ jQuery(function ($) {
             if (e != null) {
                 e.preventDefault();
             }
-            $('table.js-table').remove();
+            $('table.js-table').remove()
+            $('input.js-add-table-row').remove()
+
+            url = $(this).attr("href")
 
             if (typeof table_ == 'undefined'){
                 table_name = $(this).attr("name")
                 url = $(this).attr("href")
             } else {
                 table_name = table_
-                url = "/testapp/?model_name=" + table_name
+                // url = "/testapp/?model_name=" + table_name
             }
             data['model_name'] = table_name
-            // var $show_table = $("table[name=" + table_name + "]");
-
 
             $.ajax({
                 url: url,
@@ -55,7 +56,7 @@ jQuery(function ($) {
 
                     if (idx == 0) {
                         $.each(fields, function(key, value){
-                            $html_table.append('<th>'+key+'</th>')
+                            $html_table.append('<th name="'+key+'" class="background-grey">'+key+'</th>')
                         });
                         $html_table.append('<tbody class="js-tbody"><tr class="'+row_class_name+'">')
                         var $row = $('tr.'+row_class_name);
@@ -73,11 +74,18 @@ jQuery(function ($) {
                     }
                 });
                 $html_table.append('</tbody></table>')
+
+                $('div.content').append('<input class="js-add-table-row" type="button" value="Add New Row" />')
+                $('input.js-add-table-row').click(function() {
+                    testapp.models_preview.append_new_entry_form(data['field_types'], table_name)
+                });
+
                 $('table.js-table').on('click', 'td', testapp.models_preview.display_td_input);
               } else {
                 alert('Error!');
               }
             });
+
         },
         display_td_input:function() {
             var $cell = $(this),
@@ -110,7 +118,7 @@ jQuery(function ($) {
             if (cell.hasClass("IntegerField") || cell.hasClass("DecimalField") || cell.hasClass("FloatField")) {
                 cell.find('.newValue').numeric({ negative: false }, function() { alert("No negative values"); this.value = ""; this.focus(); });
             } else { if (cell.hasClass("DateField")) {
-                var date_input = cell.find('input.newValue'),
+                var date_input =  cell.find('input.newValue'),
                     date_parts = prevContent.match(/(\d+)/g),
                     realDate = new Date(date_parts[0], date_parts[1] - 1, date_parts[2]);
                 date_input.addClass("date-pick")
@@ -127,36 +135,55 @@ jQuery(function ($) {
         },
         ctrl_enter_press:function(e){
             if (e.ctrlKey && e.keyCode == 13) {
-                testapp.models_preview.send_table_update()
+                var model_name = $('table.js-table').attr('name');
+                testapp.models_preview.send_table_update(model_name)
             }
         },
-        send_table_update:function() {
-            var result = {},
-                data = {},
-                model_name = "";
-            $('.updated-cell').each(function(){
-                var val = "",
-                    table_row_class = $(this).closest('tr').attr('class'),
-                    object_id = table_row_class.split('-')[1],
-                    field_name = $(this).attr('name');
-                model_name = table_row_class.split('-')[0]
-                if (typeof data['model_name'] == 'undefined') {
-                    data['model_name'] = model_name
-                }
-
-                val = $(this).find('.newValue').val()
-
-                if (val == '') {
-                    val = null
-                }
-
-                if (typeof result[object_id] == 'undefined') {
-                    result[object_id] = {}
-                }
-
-                result[object_id][field_name] = val;
+        send_table_update:function(model_name) {
+            var update = {},
+                new_rows = [],
+                data = {};
+            $('tr.js-new-entry').each(function(idx, row){
+                $(row).attr('id', 'new_row-' + idx);
             });
-            data['model_data'] = JSON.stringify(result)
+            data['model_name'] = model_name
+            $('.updated-cell').each(function(idx, cell){
+                var val = "",
+                    table_row_class = $(cell).closest('tr').attr('class'),
+                    field_name = $(cell).attr('name');
+
+                if ($(cell).closest('tr').hasClass('js-new-entry') == false) {
+                    var object_id = table_row_class.split('-')[1];
+
+                    val = $(cell).find('.newValue').val()
+
+                    if (val == '') {
+                        val = null
+                    }
+                    if (typeof update[object_id] == 'undefined') {
+                        update[object_id] = {}
+                    }
+                    update[object_id][field_name] = val;
+                } else {
+                    var row_id = $(cell).closest('tr').attr('id');
+
+                    row_id = parseInt(row_id.split('-')[1])
+                    model_name = table_row_class.split(' ')[1]
+
+                    val = $(cell).find('.newValue').val()
+                    if (val == '') {
+                        val = null
+                    }
+                    if (typeof new_rows[row_id] == 'undefined') {
+                        new_rows[row_id] = {}
+                    }
+                    new_rows[row_id][field_name] = val;
+                }
+
+            });
+
+            data['update_data'] = JSON.stringify(update)
+            data['new_objects_data'] = JSON.stringify(new_rows)
             $.ajax({
                 type: "POST",
                 url: window.testapp.update_model_url,
@@ -165,7 +192,7 @@ jQuery(function ($) {
             }).done(function(data){
                 var result = data['result'],
                     table_name = "";
-                if (result == 'ok') {
+                if (result == 'success') {
                     table_name = data['model_name']
                     testapp.models_preview.click_on_model_url(null, table_name)
                 } else {
@@ -173,6 +200,28 @@ jQuery(function ($) {
                 }
             });
 
+        },
+        append_new_entry_form:function(field_types, model_name) {
+            var $table = $('table.js-table');
+                $table_body = $table.find('.js-tbody'),
+                headers = [];
+
+            $table.find('th').each(function(idx, elem) {
+                headers[idx] = $(elem).text()
+            });
+
+            $table_body.append('<tr class="js-new-entry '+model_name+'">')
+            var $new_row = $table_body.find('tr.js-new-entry:last'),
+                current_date = $.datepicker.formatDate('yy-mm-dd', new Date());
+
+            $.each(headers, function(idx, value){
+                if (field_types[value] == 'DateField') {
+                    $new_row.append('<td name="' + value + '" class="' + field_types[value] + '">'+current_date+'</td>')
+                } else {
+                    $new_row.append('<td name="' + value + '" class="' + field_types[value] + '"></td>')
+                }
+            });
+            $new_row.append('</tr>')
         }
     };
     testapp.models_preview.init();
--- testapp/views.py
+++ testapp/views.py
@@ -20,10 +20,10 @@ def preview_model_data(request, model_name=None):
             fetch_model_name = request.GET.get('model_name')
             model = get_model('testapp', fetch_model_name)
             queryset = model.objects.all()
-            output = serializers.serialize('python', queryset)
+            table_content = serializers.serialize('python', queryset)
             field_types = get_model_field_types(model)
             response.update({
-                'table_content': output,
+                'table_content': table_content,
                 'field_types': field_types,
                 'result': 'success'})
         except:
@@ -41,21 +41,34 @@ def preview_model_data(request, model_name=None):
 @ajax_request
 @require_POST
 def update_model(request):
-    response = {'result': 'ok'}
-    updated_data = None
-    model_name = request.POST.get('model_name')
-    if 'model_data' in request.POST and request.POST['model_data']:
-        updated_data = json.loads(request.POST.get('model_data'))
+    response = {'result': 'success'}
+    update_data = None
+    new_objects_data = None
+    model_name = request.POST.get('model_name', None)
+    if 'update_data' in request.POST and request.POST['update_data']:
+        update_data = json.loads(request.POST.get('update_data'))
+    if 'new_objects_data' in request.POST and request.POST['new_objects_data']:
+        new_objects_data = json.loads(request.POST.get('new_objects_data'))
 
-    if model_name and updated_data:
+    if model_name:
         response['model_name'] = model_name
+
+    requested_model = get_model('testapp', model_name)
+
+    if update_data:
         try:
-            requested_model = get_model('testapp', model_name)
-            for instance_id, new_field_vals in updated_data.iteritems():
+            for instance_id, new_field_vals in update_data.iteritems():
                 requested_model.objects.filter(
                     id=instance_id).update(**new_field_vals)
         except:
             response['result'] = 'error'
+    if new_objects_data:
+        try:
+            for new_object in new_objects_data:
+                requested_model.objects.create(**new_object)
+        except:
+            response['result'] = 'error'
+
     data = json.dumps(response, ensure_ascii=False)
 
     return HttpResponse(
