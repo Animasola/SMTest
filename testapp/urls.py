8648a8ffb7608ae5f430f9653722c2229d9913ae
animasola <annima.sola@gmail.com>
2014-06-17 18:07:56 +0300

    fixing static problem

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
 
 
@@ -5,4 +6,12 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'testapp.views.preview_model_data', name='preview_model_data'),
     url(r'^update_model/$', 'testapp.views.update_model', name='update_model'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
 )
